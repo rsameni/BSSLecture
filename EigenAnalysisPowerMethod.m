1c8748c95cf3103e87942c056ebebc56513b354d
MingzheHu <58562242+MingzheHu-Duke@users.noreply.github.com>
2021-11-07 14:23:04 -0500

    Enable Visualization

--- EigenAnalysisPowerMethod.m
+++ EigenAnalysisPowerMethod.m
@@ -1,5 +1,6 @@
-function v = EigenAnalysisPowerMethod(A, v0, N)
-% An implementation of the Power method for eigenvalue decomposition
+function [v,lambda] = EigenAnalysisPowerMethod(A, v0, N)
+% An implementation of the Power method for eigenvalue decomposition and
+% visualization
 %
 % Inputs:
 %   A: A square matrix
@@ -8,12 +9,35 @@ function v = EigenAnalysisPowerMethod(A, v0, N)
 % Output:
 %   v: An estimate of the leading eigenvalue after N iterations
 %
-% Reza Sameni (reza.sameni@gmail.com)
-% Oct 2020
- 
-v = v0(:); % initial vector
+% Mingzhe Hu
+% Nov 2021
+
+%initial vector
+v = v0(:);
+f1 = figure;
+f2 = figure;
 for k = 1 : N
+    set(0, 'CurrentFigure', f2)
+    %figure(2)
+    txt = ['Iteration = ',num2str(k)];
+    plot(v, 'DisplayName', txt);
+    title("Principle  Eigenvector")
+    legend("show", "Location", "eastoutside");
+    hold on;
     v = A * v;
+    y = A*v;
+    l=y'*v;
+    m=v'*v;
+    % eigenvalue
+    lambda =l/m;
+    % approximate the next eigenvectvor
+    %figure(1)
+    set(0, 'CurrentFigure', f1)
+    plot(k,lambda,'*b');
+    title("Principle Eigenvalue")
+    hold on;
     v = v / sqrt(v' * v);
 end
-
+disp(["Principle Eigenvalue:", num2str(lambda)]);
+disp("Principle Eigenvector:");
+disp(-v);
