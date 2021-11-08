d4847348b9c4aff706e76c82ae4c8e0aec15c0c2
Sina Dabiri <sina.dabiri@berkeley.edu>
2021-11-08 16:14:59 -0500

    adding Ex04 plot.

--- Ex04_testEOGArtifactRemoval.m
+++ Ex04_testEOGArtifactRemoval.m
@@ -21,9 +21,11 @@ switch example
     case 1 % A sample EEG from the OSET package
         load EEGdata textdata data % Load a sample EEG signal
         fs = 250;
-        x = data'; % make the data in (channels x samples) format
+        x = data';
+%         x = data([1:15 31], :)'; % make the data in (channels x samples) format
         % Check the channel names
-        disp(textdata)
+%         disp(x)
+        
     otherwise
         error('unknown example');
 end
